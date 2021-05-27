39cbe9455469a6b99eef7303fd671920f3c55ced
Michael Malloy <theshadoweeveeyt@gmail.com>
2021-05-27 00:11:58 -0500

    Add Cemu RPC Support

--- index.js
+++ index.js
@@ -84,6 +84,26 @@ bot.on("presenceUpdate", (_, presence) => {
                 console.log("No Game detected");
             }
         }
+        if (activity.name == "Cemu") {
+            currGame = activity.state.replace("Playing", "").trim();
+            if (currGame) {
+                var key = await getKey(presence.user.id);
+                if (!key) {
+                    console.log(`${presence.user.username} does not have a registered account on RiiTag.`);
+                    return;
+                }
+                var url = `http://localhost:3000/wiiu?key=${key}&game=${currGame}&source=Cemu`;
+                //console.log(url);
+                var res = await axios.get(encodeURI(url));
+                if (res.status == 200) {
+                    console.log(`${presence.user.username} is now playing ${activity.state}.`);
+                } else {
+                    console.log(`Request for ${presence.user.username} failed with response code ${res.status} for game ${activity.state}}.`);
+                }
+            } else {
+                console.log("No Game detected");
+            }
+        }
     });
 });
 
